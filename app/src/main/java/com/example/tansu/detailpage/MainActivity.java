ba106403b1ec269d8b5d275a9a010b7a8ac8585b
Sheshank-Srivastava <ambitious9350164795@gmail.com>
2018-03-15 18:11:21 +0530

    Added Description Window
    Add desciption with its content, empty activity_main

---
+++ app/src/main/java/com/example/tansu/detailpage/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tansu.detailpage;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
